487e451bcfe1237e122e10800765c0dcceb09c46
fric87 <j.f.martinez.sur@gmail.com>
2017-02-03 19:56:09 -0300

    ejercicio 8
    es el primero que subo ya que se me borraron los otros, voy a tratar de hacerlos y subirlos

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -5,4 +5,31 @@ ej.: "El resto es 0 ."*/
 function SacarResto()
 {
 	
+
+	var sueldo;
+	var descuento;
+	var resultado;
+	sueldo=document.getElementById('numeroDividendo').value;
+	descuento=document.getElementById('numeroDivisor').value;
+	sueldo=parseInt(sueldo);  
+	descuento=parseInt(descuento);
+	resultado=(sueldo%descuento);
+	alert(resultado);
+
+
+
+
+	var resultado;
+	resultado=2%10;
+	alert(resultado);
+	resultado=10%2;
+	alert(resultado);
+
+
+
+
+
+
+
+
 }

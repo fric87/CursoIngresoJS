504eaf18dbadb41521e3ebf50d680310af27abf3
fric87 <j.f.martinez.sur@gmail.com>
2017-02-03 22:09:18 -0300

    Add files via upload
    falta terminar

---
+++ jsFerreteFacturacion.js
@@ -0,0 +1,57 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+	
+
+
+var precioUno;
+var precioDos;
+var precioTres;
+var resultado;
+precioUno=document.getElementById('PrecioUno').value;
+precioDos=document.getElementById('PrecioDos').value;
+precioTres=document.getElementById('PrecioTres').value;
+precioUno=parseInt(precioUno);
+precioDos=parseInt(precioDos);
+precioTres=parseInt(precioTres);
+resultado=(precioUno+precioDos+precioTres);
+alert(resultado);
+
+
+
+//ctrl+d seleccionar varias palabras iguales. doble click sobre una palabra la selecciona y compara...
+
+
+
+
+}
+function Promedio () 
+{
+
+
+
+
+var precioUno;
+var precioDos;
+var precioTres;
+var resultado;
+precioUno=document.getElementById('PrecioUno').value;
+precioDos=document.getElementById('PrecioDos').value;
+precioTres=document.getElementById('PrecioTres').value;
+precioUno=parseInt(precioUno);
+precioDos=parseInt(precioDos);
+precioTres=parseInt(precioTres);
+resultado=((precioUno+precioDos+precioTres)/(3));
+alert(resultado);
+
+
+
+}
+function PrecioFinal () 
+{
+	
+}

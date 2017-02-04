4fb9e006aa299bc231d340ab31cf6e130ee8a99c
fric87 <j.f.martinez.sur@gmail.com>
2017-02-03 21:48:53 -0300

    Add files via upload
    terminado en clase

---
+++ jsEntradaSalida-8.js
@@ -0,0 +1,35 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resto entre el dividendo y el divisor.
+ej.: "El resto es 0 ."*/
+function SacarResto()
+{
+	
+
+	var sueldo;
+	var descuento;
+	var resultado;
+	sueldo=document.getElementById('numeroDividendo').value;
+	descuento=document.getElementById('numeroDivisor').value;
+	sueldo=parseInt(sueldo);  
+	descuento=parseInt(descuento);
+	resultado=(sueldo%descuento);
+	alert(resultado);
+
+
+
+
+	var resultado;
+	resultado=2%10;
+	alert(resultado);
+	resultado=10%2;
+	alert(resultado);
+
+
+
+
+
+
+
+
+}

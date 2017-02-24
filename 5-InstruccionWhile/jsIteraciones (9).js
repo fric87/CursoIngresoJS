8e714607504eacc8ebafff8e073b909380362cf2
fric87 <j.f.martinez.sur@gmail.com>
2017-02-24 15:30:16 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,8 +3,8 @@ function Mostrar()
 
 	var contador=0;
 	var edad;
-	var maximo;
-	var minimo;
+	var edadMaxima;
+	var edadMinima;
 	var nombre;
 	var nombreDelMayor;
 	var sexo;
@@ -22,80 +22,81 @@ function Mostrar()
 	
 	var respuesta='si';
 
-		while(respuesta!='no')
+		while(respuesta!='NO')
 		{
-			edad=prompt("ingrese edad");
-			edad=parseInt(edad);
+			nombre=prompt("por favor ingrese nombre");                              //nombre
+			edad=prompt("ingrese edad");                             				//edad
+			edad=parseInt(edad);													
 
-			while(edad<0||edad>100)
-			{
-				edad=prompt("edad invalida, por favor reingrese edad");
-				edad=parseInt(edad);
-			}
+					 while(edad<0||edad>100)
+					{
+					 	edad=prompt("edad invalida, por favor reingrese edad");
+						edad=parseInt(edad);
+					}
 
-				sexo=prompt("por favor ingrese su sexo");
+			sexo=prompt("por favor ingrese su sexo");							//sexo
 
-				while(sexo!="f"&&sexo!="m")
-				{
-					sexo=prompt("sexo invalido, por favor ingrese f o m" );
-				}
+					while(sexo!="f"&&sexo!="m")
+					{
+						sexo=prompt("sexo invalido, por favor ingrese f o m" );
+					}
 
-				nota=prompt("ingresar nota");
-				nota=parseInt(nota);
+			nota=prompt("ingresar nota");
+			nota=parseInt(nota);
 
-				while(nota<0||nota>10)
-				{
-					nota=prompt("reingrese una nota entre 0 y 10");
-					nota=parseInt(nota);
 
-				}
-					if(nota=="10"&&bandera==nadie)
+					while(nota<0||nota>10)
+					{
+						nota=prompt("reingrese una nota entre 0 y 10");
+						nota=parseInt(nota);
+
+					}
+					
+			if(nota=="10"&&bandera=="nadie")     					 //primer persona q se saco 10
 					{
 					nombreDiez=nombre;
 					sexoDiez=sexo;
 					edadDiez=edad;
-					bandera="nada";	           //nombre sexo y edad
+					bandera="cambie la bandera para q sea el primero";	           
 					}
 
 
-				//cantidad de personas aprobadas
+				
 
-			contador++;
-			nombre=prompt("por favor ingrese nombre");
+					contador++;   									//cantidad de personas aprobadas
+					
 
-				if(contador==1)
-				{
-					maximo=edad;
-					minimo=edad;
-				}
-		else
-		{
-			if(edad=maximo)
-			{
-				maximo=edad;
-				nombreDelMayor=nombre;
-			}
+						if(contador==1)
+						{
+							edadMaxima=edad;
+							edadMinima=edad;
+						}
+						else
+						{
+							if(edad>edadMaxima)
+							{
+								edadMaxima=edad;
+								nombreDelMayor=nombre;
+							}
 
-			if(edad=minimo)
-			{
-				minimo=edad;
-			}
+							if(edad<edadMinima)
+							{
+								edadMinima=edad;
+							}
 
-		}
+						}
 		
 				
-				if(contador==1)
+				if(contador==1)    //nota minima y sexo de la nota minima
 				{
-					contador++;
+					
 					notaMinima=nota;
-
-
 				}
 
 				else
 
 				{
-					if(nota=notaMinima)
+					if(nota<notaMinima)
 					{
 						notaMinima=nota;
 						sexoNotaMinima=sexo;
@@ -116,7 +117,7 @@ function Mostrar()
 		respuesta=prompt("para salir teclee NO");
 		}
 
-
+//pedir nota
 //verifivar si aprobo
 //cantidad de mujeres que aprobaron con nota par y son menores de edad
 }//FIN DE LA FUNCIÓN

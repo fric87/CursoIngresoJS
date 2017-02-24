638538c3d2aa13c70399288d4dea3f8282191c11
fric87 <j.f.martinez.sur@gmail.com>
2017-02-24 17:12:13 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -5,48 +5,88 @@ var mascota=0;
 var contadorSi=0;
 var contadorNo=0;
 var edad=0
-var edadaxima;
+var edadMaximaSinGato;
+var nombre;
+var nombreDelGato;
+var sexo;
+var edadDelGato;
 
-for(mascota=0;contador<1000;contador++)
+
+for(contador=0;contador<1000;contador++)
 	{
-		mascota=prompt("tenes massscota");
-		edad=prompt("que edad tiene");
+
+		nombre=prompt("como te llamas??");
+		edad=prompt("que edad tenes");                                 //pregunta edad
 		edad=parseInt(edad);
 		if(edad<0||edad>100)
 		{
 			edad=prompt("ingrese una numero de 1 a 100");
 		}
 
-	}
-
-	if(mascota="si")
-	{
-		contadorSi=contador++
-
-		mascota=prompt("que tipo de mascota tenes");
-
-			if(mascota|="perro"&&mascota|="gato")
+		sexo=prompt("ingrese su sexo... (F o M) ");
+			if(sexo!="F"&&sexo!="M")
 			{
-				mascota=prompt("ingrese perro o gato");
+				sexo=prompt("por favor ingrese solo F o M ");
+			}
+		
+		mascota=prompt("tenes massscota");                               //pregunta massscota
+		
+
+	
+
+			if(mascota=="si")  												//mascota si
+			{
+				contadorSi=contadorSi+1;
+
+				mascota=prompt("que tipo de mascota tenes");
+
+					if(mascota!="perro"&&mascota!="gato")
+					{
+						mascota=prompt("ingrese perro o gato");
+					}
+
+					if(mascota=="gato")
+					{
+						nombreDelGato=prompt("como se llama su gatito");
+						edadDelGato=prompt("que edad tiene su gatito");
+						edadDelGato=parseInt(edadDelGato);
+							if(edadDelGato<0||edadDelGato>20)
+								edadDelGato=prompt("ningun gato vive tanto ingrese un numero de 0 a 20");
+					}
+
+			}
+
+
+			else															//mascota no
+			{
+				contadorNo=contadorNo+1;
+
+				if(contadorNo==1)
+				{
+					edadMaximaSinGato=edad;
+				}
+
+						else
+						{
+							if(edad>edadMaximaSinGato)
+							{
+								edadMaximaSinGato=edad;							//edad maxima q no tiene gato
+							}
+						}
+
+
 			}
 
 
 
-	}
+			if(sexo=="F"&&edad>30&&edadDelGato<6)
+			{
+				alert("el nombre del gato es "+nombreDelGato);
+			}
 
-	else
-	{
-		contadorNo=contador++
-
-		if(edad==0)
-		{
-			edadMax=edad
-		}
-
-
-
-	}
 	
+	}
+
 
 }
 //necesito saber el nombre del primer gato que sea de una mujer mayor a 30 años y el gato no tenga mas de 5 años de edad.
